7b25513ee97cf68c63b9ebbf45f8aeb54ac87393
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-30 00:11:01 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    #GET /users/:id
+    def show
+        user=User.find(params[:id])
+        render json: user,include: :items
+    end
+
+    private
+
+    def render_not_found_response
+      render json: { error: "Dog house not found" }, status: :not_found
+    end
 end
